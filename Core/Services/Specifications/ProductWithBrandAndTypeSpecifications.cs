bdf35456c951aa2c98e6a623860a8fff5761d049
DiabloXV <haboali1001@gmail.com>
2024-11-06 06:02:42 +0200

    #Specifications

---
+++ Core/Services/Specifications/ProductWithBrandAndTypeSpecifications.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace Services.Specifications
+{
+    internal class ProductWithBrandAndTypeSpecifications : Specifications<Product>
+    {
+        //Used to retrieve product By Id
+        public ProductWithBrandAndTypeSpecifications(int id) : base(product => product.Id == id)
+        {
+            AddInclude(product => product.ProductBrand);
+            AddInclude(product => product.ProductType);
+        }
+        //Used to get all products
+        public ProductWithBrandAndTypeSpecifications() : base(null)
+        {
+            AddInclude(product => product.ProductBrand);
+            AddInclude(product => product.ProductType);
+        }
+    }
+}
